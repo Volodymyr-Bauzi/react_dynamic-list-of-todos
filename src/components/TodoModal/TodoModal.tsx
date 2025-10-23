e7034becb386494cf69814077dd3de22719b25f7
Bauzi <volodiasyd@gmail.com>
2025-10-23 18:15:39 +0300

    fix: changed static mailto link to dynamic from user

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -68,7 +68,7 @@ export const TodoModal = ({ todo, onModalClose }: TodoModalProps) => {
 
               {' by '}
 
-              <a href="mailto:Sincere@april.biz">{user?.name}</a>
+              <a href={`mailto:${user?.email}`}>{user?.name}</a>
             </p>
           </div>
         </div>
